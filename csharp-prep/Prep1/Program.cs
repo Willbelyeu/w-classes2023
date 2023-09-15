69ae8a78b7a255a1045f04726c07be923dcc8d47
Willbelyeu <114535243+Willbelyeu@users.noreply.github.com>
2023-09-15 13:53:17 -0600

    Finished first task

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        string firstname;
+        string lastname;
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?");   
+        firstname=Console.ReadLine();
+        Console.Write("What is your last name?");
+        lastname=Console.ReadLine();
+        Console.Write($"Your name is {lastname}, {firstname} {lastname}.");
     }
+
 }
