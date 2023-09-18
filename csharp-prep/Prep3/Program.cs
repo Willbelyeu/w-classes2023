4f51cc493f3f28a4e6f6c261d10bb7e9e750fa88
Willbelyeu <114535243+Willbelyeu@users.noreply.github.com>
2023-09-18 13:38:08 -0600

    finished number 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator=new Random();
+        int number= randomGenerator.Next(1,11);
+        int guessNumber=21;
+
+        while (guessNumber!=number) 
+        {         
+            Console.Write("What do you think the number is?");
+            guessNumber=int.Parse(Console.ReadLine());
+
+        
+            if (guessNumber>number)
+            {
+                Console.WriteLine("Your guess is larger than the number.");
+            }
+            else if (guessNumber==number)
+            {
+                Console.WriteLine("That is correct.");
+            }
+            else if (guessNumber<number)
+            {
+                Console.WriteLine("Your guess was smaller than the number.");
+            }
+        }
+            
     }
 }
