bc5e9ce6b49314a134a29048e1dd9136e85ce61c
Willbelyeu <114535243+Willbelyeu@users.noreply.github.com>
2023-09-20 16:38:21 -0600

    finished prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        void WelcomeToProgram()
+        {
+            Console.WriteLine("Welcom to program!");
+        }
+        string GetUserName()
+        {
+            Console.WriteLine("What is your name?");
+            string YourName=Console.ReadLine();
+            return YourName;
+        }
+        int GetUserNumber()
+        {
+            Console.WriteLine("What is your favorite number?");
+            int YourNumber=int.Parse(Console.ReadLine());
+            return YourNumber;
+        }
+        int SquareFavoriteNumber(int number)
+        {
+            int SquareNumber=number*number;
+            return SquareNumber;
+        }
+        static void DisplayResults(string YourName, int SquareNumber)
+        {
+            Console.WriteLine($"{YourName}, the square of your number is {SquareNumber}.");
+        }
+        WelcomeToProgram();
+        string YourName=GetUserName();
+        int YourNumber=GetUserNumber();
+        int SquareNumber=SquareFavoriteNumber(YourNumber);
+        DisplayResults(YourName,SquareNumber);
     }
 }
