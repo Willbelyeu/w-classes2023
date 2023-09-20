704fa456f43b994615923afe4801604f2589e97c
Willbelyeu <114535243+Willbelyeu@users.noreply.github.com>
2023-09-20 09:47:47 -0600

    finished number4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Schema;
+using Microsoft.VisualBasic;
 
 class Program
-{
+{        
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        int response=1;
+        int total=0;
+        int highest=0;
+        int average=0;
+        while (response!=0)
+        {
+            Console.WriteLine("Type a number to add to the list.");
+            response=int.Parse(Console.ReadLine());
+            if (response!=0)
+            {
+                numbers.Add(response);
+                total+=response;
+                if (response>=highest)
+                {
+                    highest=response;
+                }
+                foreach (int number in numbers)
+                {  
+                    Console.Write($"{number}, ");                
+                }
+            }
+            else
+            {
+                average=total/numbers.Count;
+                Console.WriteLine ($"sum total is {total}");
+                Console.WriteLine ($"Highest number is {highest}");
+                Console.WriteLine ($"Average of numbers is {average}");
+            }
+        }
     }
 }
