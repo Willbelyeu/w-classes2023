c6594458e1f97b664f6b8827c71a3375edf39af0
Willbelyeu <114535243+Willbelyeu@users.noreply.github.com>
2023-09-27 18:59:14 -0600

    Fiinished what was due Sep27

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1=new Job();
+        job1._company="mycrosoft";
+        job1._jobTitle="software engineeer";
+        job1._startYear=1997;
+        job1._endYear=2004;
+        
+        Job job2=new Job();
+        job2._jobTitle="hardware engineeer";
+        job2._company="apple";
+        job2._startYear=1999;
+        job2._endYear=2016;
+
+        Resume myResume=new Resume();
+        myResume._name="Robert Sanders";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
