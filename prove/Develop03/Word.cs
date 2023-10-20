b14d6376faf82fb10bf721b07a4f73a50778f6b3
Willbelyeu <114535243+Willbelyeu@users.noreply.github.com>
2023-10-20 15:58:57 -0600

    tried to put a don't use used words. Failed.

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -8,11 +8,24 @@ private string scriptureModified = "";
     int topValue = words.Count();
     Random rnd = new Random();
     int num = rnd.Next(0, topValue);
+    //below trying to set up a loop to check if a word is already removed.
+    List<int> calledOptions= new List<int>();
+    if (calledOptions.Count()==words.Count()){
+        Console.WriteLine("finished. All words removed.");
+    }
+    else if (calledOptions.Contains(num)){
+        num=rnd.Next(0, topValue);
+    }
+    else {
+        calledOptions.Add(num);
+    }
+    //above trying to set up a loop to check if a word is already removed. Tests are not successful. Unable to find a system.
     int letterCount = words[num].Length;
+
     string blankLine = "";
         for (int i=0; i<letterCount; i++){
             blankLine+="_";
-        }
+         }
 
 words[num] = blankLine;
 foreach (string word in words)
