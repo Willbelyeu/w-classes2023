1a0be24aa71e4d2fc690150b51b9c0d916c9bcdb
Willbelyeu <114535243+Willbelyeu@users.noreply.github.com>
2023-10-25 12:05:23 -0600

    Finished Inheritance learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Runtime.Versioning;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        WrittingAssignment writtingAssignment= new WrittingAssignment("Samuel Bennett", "Eauropean History", "An irelivent string");
+        string summaryWritting=writtingAssignment.GetSummary();
+        string writtingSummary=writtingAssignment.GetWrittingInformation();
+        Console.WriteLine($"{summaryWritting}");
+        Console.WriteLine($"{writtingSummary}");
+
+        MathAssignment mathAssignment= new MathAssignment("Samuel Bennett", "fractions", "section 7.3", "Problems 8-19");
+        string summaryMath=mathAssignment.GetSummary();
+        string mathSummary=mathAssignment.GetHomeworkList();
+        Console.WriteLine($"{summaryMath}");
+        Console.WriteLine($"{mathSummary}");
     }
 }
